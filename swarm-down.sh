0bcf06a5279a11c4eafad16dd702da81145aee15
Stefan Prodan <stefan.prodan@gmail.com>
2016-10-18 11:02:36 +0300

    swarm setup

---
+++ swarm-down.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+set -e
+
+image="eventlog"
+
+docker service scale $image=0
+docker service scale ${image}-proxy=0
+docker service scale ${image}-ping=0
+
+docker service rm $image ${image}-proxy ${image}-ping
+
+docker rmi -f $image
---
+++ swarm-up.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+set -e
+
+image="eventlog"
+network="eventlog-net"
+
+# build image
+if [ ! "$(docker images -q  $image)" ];then
+	docker pull microsoft/dotnet:latest
+    docker build -t $image .
+fi
+
+# create network
+if [ ! "$(docker network ls --filter name=$network -q)" ];then
+    docker network create --driver overlay $network
+fi
+
+# run containers
+docker service create --network $network -p 5020:5000 --name ${image} --restart unless-stopped $image
+docker service create --network $network -p 5010:5000 --name ${image}-proxy --restart unless-stopped -e PROXY_FOR='http://eventlog:5000' $image
+docker service create --network $network -p 5030:5000 --name ${image}-ping --restart unless-stopped -e PING_INTERVAL="100" -e PING_TARGETS='http://eventlog:5000,http://eventlog-proxy:5000' $image
