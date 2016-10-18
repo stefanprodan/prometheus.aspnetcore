a4f00e65f7c27a6d1e6a121949da742d6da20b86
Stefan Prodan <stefan.prodan@gmail.com>
2016-10-18 11:04:18 +0300

    fix swarm service create

--- swarm-up.sh
+++ swarm-up.sh
@@ -16,6 +16,6 @@ if [ ! "$(docker network ls --filter name=$network -q)" ];then
 fi
 
 # run containers
-docker service create --network $network -p 5020:5000 --name ${image} --restart unless-stopped $image
-docker service create --network $network -p 5010:5000 --name ${image}-proxy --restart unless-stopped -e PROXY_FOR='http://eventlog:5000' $image
-docker service create --network $network -p 5030:5000 --name ${image}-ping --restart unless-stopped -e PING_INTERVAL="100" -e PING_TARGETS='http://eventlog:5000,http://eventlog-proxy:5000' $image
+docker service create --network $network -p 5020:5000 --name ${image} $image
+docker service create --network $network -p 5010:5000 --name ${image}-proxy -e PROXY_FOR='http://eventlog:5000' $image
+docker service create --network $network -p 5030:5000 --name ${image}-ping -e PING_INTERVAL="100" -e PING_TARGETS='http://eventlog:5000,http://eventlog-proxy:5000' $image
